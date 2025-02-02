7d59792cbec6ee9e5371aa12b4c20ea6302384fd
Khem <khemchandsh3@gmail.com>
2025-02-03 00:48:44 +0530

    left rotate by D using different approach

--- Content/src/DSA/Problem_Solving/Easy/Array/LeftRotateByDPlaces.java
+++ Content/src/DSA/Problem_Solving/Easy/Array/LeftRotateByDPlaces.java
@@ -2,11 +2,14 @@ package DSA.Problem_Solving.Easy.Array;
 // left rotate the array by D places.
 
 import DSA.Data_Structures.Array;
+import com.sun.security.jgss.GSSUtil;
 
 import java.util.Arrays;
 
 public class LeftRotateByDPlaces {
-    public static void leftRotate(int[] arr){
+    /*
+   I Approach Naive approach : O(d*n)
+    public static void leftRotateUsingNestedLoop(int[] arr){
         int n = arr.length;
         for(int i=1;i<n;i++){
             int temp = arr[i];
@@ -16,14 +19,65 @@ public class LeftRotateByDPlaces {
 
         System.out.println(Arrays.toString(arr));
     }
+*/
+
+/*
+  II Approach : O(N+3) roughly and O(d) extra space
+
+    public static void leftRotateUsingTempArray(int[] arr,int d){
+        int n = arr.length;
+        int[] tempArray = new int[d];
+        for(int i=0;i<d;i++){
+            tempArray[i] = arr[i];
+        }
+
+        for(int i = 0;i<n-d;i++){
+            arr[i] = arr[d+i];
+        }
+
+        for(int i=0;i<tempArray.length;i++){
+            arr[n-d+i] = tempArray[i];
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+ */
+
+    public static void LeftRotateUsingReverse(int[] arr, int start,int end){
+        end--;
+        while (start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        };
+    }
+
 
     public static void main(String[] args) {
         int[] arr = new int[]{1,2,3,4,5};
+        int n = arr.length;
         int d = 3;
-
+        int start = 0;
+       /* I Approach
         for(int i=0;i<d;i++){
-            leftRotate(arr);
+            leftRotateUsingNestedLoop(arr);
         }
+
+        */
+
+        /*
+         II Approach
+       leftRotateUsingTempArray(arr,d);
+
+         */
+
+        // III Approach : using reverse method
+        LeftRotateUsingReverse(arr,start,d);
+        LeftRotateUsingReverse(arr,d,n);
+        LeftRotateUsingReverse(arr,start,n);
         System.out.println(Arrays.toString(arr));
+
     }
 }
