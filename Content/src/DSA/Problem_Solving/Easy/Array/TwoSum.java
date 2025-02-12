67167763893481a5b02c22ef314646b639d5e68b
Khem <khemchandsh3@gmail.com>
2025-02-13 00:55:45 +0530

    two sum optimized approach solution

--- Content/src/DSA/Problem_Solving/Easy/Array/TwoSum.java
+++ Content/src/DSA/Problem_Solving/Easy/Array/TwoSum.java
@@ -1,11 +1,17 @@
 package DSA.Problem_Solving.Easy.Array;
-
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 // return the index of two elements whose sum is equal to k
 public class TwoSum {
     public static void main(String[] args) {
         int[] arr = new int[]{1,0,7,7,1};
-        int k = 14;
-        int n = arr.length;
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(arr, target)));
+        /*
+        Naive approach : for every element first find the complement by subtracting the current element from the complement and check if the complement exists for the rest of the element present in the array
+        Time complexity : O(N^2)
+        Space complexity : O(1)
         boolean pairFound = false;
         for(int i=0;i<n;i++){
             int res = k -arr[i]; // k - arr[i] = arr[j]
@@ -24,5 +30,23 @@ public class TwoSum {
         if(pairFound==false) {
             System.out.println("pair not found");
         }
+
+         */
+
+
+    }
+
+    static int[] twoSum(int[] arr, int target){
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i =0;i<arr.length;i++){
+            int complement = target - arr[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
+            }
+            map.put(arr[i], i);
+
+        }
+       return new int[]{-1,-1};
     }
 }
